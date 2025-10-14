6f1f9e755f4c4f83079cb578fbcf193462b27d0a
Lidizz <lidor87@gmail.com>
2025-10-14 22:48:14 +0200

    added forms, register and login + requirement.txt (and a temp.txt) file

---
+++ forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+# The RegistrationForm class defines a registration form with fields for username, email, password, confirm password, and a submit button.
+class RegistrationForm(FlaskForm):
+    # Verify user enters a username and between 2 and 20 characters
+    username = StringField('Username',validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+# The LoginForm class defines a login form with fields for email, password, remember me checkbox, and a submit button.
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
