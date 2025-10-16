fbb6ef1d9ae473d130a6da2cb7c5413722e1c6fb
Lidizz <lidor87@gmail.com>
2025-10-16 19:04:36 +0200

    refactored

--- flaskblog.py
+++ flaskblog.py
@@ -31,6 +31,7 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        # flash allows us to show messages to the user and 'success' is a category for styling from bootstrap
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
@@ -38,6 +39,12 @@ def register():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
